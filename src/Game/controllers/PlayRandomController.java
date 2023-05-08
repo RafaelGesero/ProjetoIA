f9eb98a65ab6f9d9e9fdae24c997633c38bd8819
RafaelGesero <racgo@iscte-iul.pt>
2023-05-08 18:27:17 +0100

    Base do projeto

---
+++ src/Game/controllers/PlayRandomController.java
@@ -0,0 +1,12 @@
+package Game.controllers;
+
+import java.util.Random;
+
+import Game.space.SpaceInvaders;
+
+public class PlayRandomController {
+	public static void main(String[] args) {
+		GameController c = new RandomController(new Random());
+		SpaceInvaders.showControllerPlaying(c,5);
+	}
+}
